c5a974fdc9f2db55f475575b3926c7c04b3ef83d
nayanborse <nayanborse34171@gmail.com>
2023-05-27 13:16:42 +0530

    Add fields in pojo

--- src/main/java/com/student/demo/model/Student.java
+++ src/main/java/com/student/demo/model/Student.java
@@ -12,17 +12,25 @@ public class Student {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
+	private String mobile;
+	private String email;
+	private Integer rollNo;
 	private String city;
 	
 	public Student() {
 		
 	}
 	
-	public Student(int id, String name, String city) {
+	public Student(int id, String name, String mobile, String email, Integer rollNo, String city) {
+		super();
 		this.id = id;
 		this.name = name;
+		this.mobile = mobile;
+		this.email = email;
+		this.rollNo = rollNo;
 		this.city = city;
 	}
+
 	public int getId() {
 		return id;
 	}
@@ -40,6 +48,30 @@ public class Student {
 	}
 	public void setCity(String city) {
 		this.city = city;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getRollNo() {
+		return rollNo;
+	}
+
+	public void setRollNo(Integer rollNo) {
+		this.rollNo = rollNo;
 	}
 	
 	
--- src/test/java/com/student/demo/StudentApplicationTests.java
+++ src/test/java/com/student/demo/StudentApplicationTests.java
@@ -1,39 +1,8 @@
 package com.student.demo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import com.student.demo.model.Student;
-import com.student.demo.repository.StudentRepository;
-import com.student.demo.service.StudentService;
 
 @SpringBootTest
 class StudentApplicationTests {
-
-	@Autowired
-	private StudentService studentService;
-
-	@MockBean
-	private StudentRepository studentRepository;
 	
-	@Test
-	public void saveStudentTest(){
-		Student student = new Student(1, "Jeet", "Kolhapur");
-		when(studentRepository.save(student)).thenReturn(student);
-		Student stud = studentService.saveStudent(student);
-		assertEquals(student, stud);		
-	}
-	
-	@Test
-	public void getStudent() {
-		Student student = new Student(1, "Jeet", "Kolhapur");
-		when(studentRepository.findById(1)).thenReturn(student);
-		Student stud = studentService.getStudent(1);
-		assertEquals("Jeet", stud.getName());
-	}
 }
